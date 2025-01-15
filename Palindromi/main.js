b7bd53f1b1b7c29c3afd3fceb378270ae10c42df
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-15 12:31:00 +0100

    ADD: reversed word variable

--- Palindromi/main.js
+++ Palindromi/main.js
@@ -1,1 +1,10 @@
-let word = prompt("Scrivi una parola.").toLowerCase()
+let word = prompt("Scrivi una parola.")
+
+function verifyPal(x) {
+    let reversedWord = word.toLowerCase().split("").reverse().join("");
+
+
+}
+
+let reversedWord = word.toLowerCase().split("").reverse().join("");
+console.log(reversedWord)
