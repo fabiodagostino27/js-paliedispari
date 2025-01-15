0a5890faeec85032dd909f938bb1a797367023b2
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-15 12:52:56 +0100

    ADD: 2 prompts

---
+++ Pari e dispari/main.js
@@ -0,0 +1,4 @@
+let userChoice = parseInt(prompt("Inserisci 0 se scommetti su pari e 1 se scommetti su dispari."));
+let userNum = parseInt(prompt("Ora scegli un numero da 1 a 5."));
+
+console.log(userChoice, userNum)
