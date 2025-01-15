5b8f7e08e20607978af62872e63ca8876116307c
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-15 12:58:35 +0100

    ADD: random number function

--- Pari e dispari/main.js
+++ Pari e dispari/main.js
@@ -1,4 +1,11 @@
 let userChoice = parseInt(prompt("Inserisci 0 se scommetti su pari e 1 se scommetti su dispari."));
 let userNum = parseInt(prompt("Ora scegli un numero da 1 a 5."));
+let pcNum;
+let min = 1;
+let max = 5;
 
-console.log(userChoice, userNum)
+function randomNum(x, y) {
+    pcNum = Math.floor(Math.random() * y) + x;
+
+    return pcNum
+}
